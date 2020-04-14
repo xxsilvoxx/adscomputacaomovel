6ddbdcec5e3b652fe87b785bca840fb01aa99dea
Lucas Henrique de Abreu <lucas.abreu@softfocus.com.br>
2020-04-14 19:53:40 -0300

    tela de usuario'

--- marvelcatalogo/lib/pages/usuario.dart
+++ marvelcatalogo/lib/pages/usuario.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-class Usuario extends StatelessWidget {
+class Usuario extends StatefulWidget {
   const Usuario({Key key}) : super(key: key);
+
+  @override
+  _UsuarioState createState() => _UsuarioState();
+}
+
+class _UsuarioState extends State<Usuario> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +17,37 @@ class Usuario extends StatelessWidget {
       appBar: AppBar(
         title: Text('Usuário'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Informe seu username da API dar Marvel',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, informe seu username, ele é obrigatório.';
+                  }
+                  return null;
+                },
+              ),
+              RaisedButton(
+                child: Text('Salvar'),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    print('Tudo ok');
+                  } else {
+                    print('Formulário com inválido');
+                  }
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
